774fed46a6a0fe3f3c6196f48bca984c34228b7b
Andrew Gwozdziewycz <git@apgwoz.com>
2021-09-30 09:17:10 -0700

    We proved we can do < 200 loc. Format it now.

--- nibl.c
+++ nibl.c
@@ -1,28 +1,54 @@
 #include <stdlib.h>
 #include "nibl.h"
 /* don't reset mouse, since state is  */
-#define RESET_NIBL(n) do { n->stopped = 0; n->t = 0; \
+#define RESET_NIBL(n) do { n->stopped = 0; n->t = 0;                    \
     n->fgr = n->fgg = n->fgb = n->fga = 0; n->bgr = n->bgg = n->bgb = n->bga = 0; \
     memset(n->keyboard, 0, sizeof(n->keyboard)); } while (0);
 #define KEY_BUFFER_SIZE 32
-struct nibl_key { SDL_Keycode code; int32_t counter; };
+struct nibl_key {
+  SDL_Keycode code;
+  int32_t counter;
+};
+
 struct Nibl {
   void *(*setup)(struct Nibl *n);
   int32_t (*draw)(struct Nibl *n, void *userdata);
-  SDL_Window *window; SDL_Renderer *renderer;
-  uint8_t fgr; uint8_t fgg; uint8_t fgb; uint8_t fga;   uint8_t bgr; uint8_t bgg; uint8_t bgb; uint8_t bga; /* color state */
-  uint64_t t; uint8_t stopped; uint8_t fps; /* epoch, stopped */
-  int32_t mx; int32_t my; int32_t mxr; int32_t myr; int32_t mpressed; /* mouse x, y, relx, rely, pressed state */
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  /* foreground and background color state */
+  uint8_t fgr; uint8_t fgg; uint8_t fgb; uint8_t fga;
+  uint8_t bgr; uint8_t bgg; uint8_t bgb; uint8_t bga;
+  uint64_t t;
+  uint8_t stopped;
+  uint8_t fps;
+  /* mouse state */
+  int32_t mx; int32_t my; int32_t mxr; int32_t myr; int32_t mpressed;
+  /* keyboard state */
   struct nibl_key keyboard[KEY_BUFFER_SIZE];
-  void *userdata; /* provided by setup, passed to draw */
+  /* userdata that we pass to draw and get from setup */
+  void *userdata;
 };
-static void panic(char *msg) {
+
+static void
+panic(char *msg) {
   fprintf(stderr, "%s", msg);
   exit(1);
 }
-static int32_t processEvents(struct Nibl *n) {
-  SDL_Event event; int32_t err; uint32_t framestart; uint32_t frameticks; int keyplace;
+
+static int32_t
+processEvents(struct Nibl *n)
+{
+  SDL_Event event;
+  int32_t err;
+  uint32_t framestart;
+  uint32_t frameticks;
+  int keyplace; /* placement for keyboard state array */
   n->mx = n->my = n->mxr = n->myr = n->mpressed = 0; /* reset event state */
+
+  if (n->t == 0 && n->setup != NULL) { /* run setup on first frame */
+    n->userdata = n->setup(n);
+  }
+
   framestart = SDL_GetTicks();
   while (SDL_PollEvent(&event)) {
     if (event.type == SDL_KEYDOWN) {
@@ -30,40 +56,49 @@ static int32_t processEvents(struct Nibl *n) {
         if (n->keyboard[i].code == 0) { keyplace = i; }
         if (n->keyboard[i].code == event.key.keysym.sym) { keyplace = i; break; }
       }
-      n->keyboard[keyplace].code = event.key.keysym.sym; n->keyboard[keyplace].counter++;
-    } else if (event.type == SDL_KEYUP) {
+      n->keyboard[keyplace].code = event.key.keysym.sym;
+      n->keyboard[keyplace].counter++;
+    }
+    else if (event.type == SDL_KEYUP) {
       for (int i = 0; i < KEY_BUFFER_SIZE; i++) {
         if (n->keyboard[i].code == event.key.keysym.sym) {
           n->keyboard[i].code = n->keyboard[i].counter = 0; break;
         }
       }
-    } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+    }
+    else if (event.type == SDL_MOUSEBUTTONDOWN) {
       n->mpressed |= SDL_BUTTON(event.button.button) & 0x7;
       n->mx = event.motion.x/NIBL_ZOOM; n->my = event.motion.y/NIBL_ZOOM;
-    } else if (event.type == SDL_MOUSEBUTTONUP) {
+    }
+    else if (event.type == SDL_MOUSEBUTTONUP) {
       n->mpressed &= ~(SDL_BUTTON(event.button.button)) & 0x7;
       n->mx = event.motion.x/NIBL_ZOOM; n->my = event.motion.y/NIBL_ZOOM;
-    } else if (event.type == SDL_MOUSEMOTION) {
+    }
+    else if (event.type == SDL_MOUSEMOTION) {
       n->mx = event.motion.x/NIBL_ZOOM; n->my = event.motion.y/NIBL_ZOOM; n->mxr = event.motion.xrel/NIBL_ZOOM; n->myr = event.motion.yrel/NIBL_ZOOM;
-    } else if (event.type == SDL_QUIT) {
+    }
+    else if (event.type == SDL_QUIT) {
       exit(0);
     }
+
     if (SDL_PeepEvents(&event, 1, SDL_PEEKEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT)) {
-      if (event.type == SDL_KEYUP || event.type == SDL_MOUSEBUTTONUP) { break; } /* notice fast up/down events */
+      /* notice fast up/down events */
+      if (event.type == SDL_KEYUP || event.type == SDL_MOUSEBUTTONUP) { break; }
     }
   }
-  if (n->t == 0) { /* run setup on first frame */
-    n->userdata = n->setup(n);
-  }
+
   SDL_RenderClear(n->renderer);
   err = n->draw(n, n->userdata);
   SDL_RenderPresent(n->renderer);
-  n->t++; frameticks = SDL_GetTicks() - framestart;
+  n->t++;
+  frameticks = SDL_GetTicks() - framestart;
   int32_t delay = (1000 / NIBL_FPS) - frameticks;
   if (delay > 0) { SDL_Delay((uint32_t)delay); }
   return err;
 }
-struct Nibl *nibl_new(void *(*setup)(struct Nibl *), int32_t (*draw)(struct Nibl *, void *)) {
+
+struct Nibl *
+nibl_new(void *(*setup)(struct Nibl *), int32_t (*draw)(struct Nibl *, void *)) {
   struct Nibl *n = malloc(sizeof(*n));
   if (n == NULL) { panic("unable to allocate Nibl context"); }
   RESET_NIBL(n);
@@ -72,10 +107,16 @@ struct Nibl *nibl_new(void *(*setup)(struct Nibl *), int32_t (*draw)(struct Nibl
   n->draw = draw;
   return n;
 }
-void nibl_destroy(struct Nibl *n) {
+
+void
+nibl_destroy(struct Nibl *n)
+{
   if (n) { free(n); }
 }
-void nibl_start(struct Nibl *n) {
+
+void
+nibl_start(struct Nibl *n)
+{
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) { panic("unable to initialize SDL\n"); }
   /* get a window. get everything setup. */
   n->window = SDL_CreateWindow("Nibl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, NIBL_WIDTH*NIBL_ZOOM, NIBL_HEIGHT*NIBL_ZOOM, SDL_WINDOW_SHOWN);
@@ -86,39 +127,71 @@ void nibl_start(struct Nibl *n) {
   n->fps = NIBL_FPS;
   while (!processEvents(n));
 }
-void nibl_stop(struct Nibl *n) {
+
+void
+nibl_stop(struct Nibl *n)
+{
   SDL_DestroyRenderer(n->renderer);
   SDL_DestroyWindow(n->window);
   SDL_Quit();
 }
-int32_t nibl_mouse(struct Nibl *n, int32_t *x, int32_t *y, int32_t *xrel, int32_t *yrel) {
+
+int32_t
+nibl_mouse(struct Nibl *n, int32_t *x, int32_t *y, int32_t *xrel, int32_t *yrel)
+{
   if (x != NULL) { *x = n->mx; }
   if (y != NULL) { *y = n->my; }
   if (xrel != NULL) { *xrel = n->mxr; }
   if (yrel != NULL) { *yrel = n->myr; }
   return n->mpressed;
 }
-int32_t nibl_keyboard(struct Nibl *n, SDL_Keycode code) {
+
+int32_t
+nibl_keyboard(struct Nibl *n, SDL_Keycode code)
+{
   for (int i = 0; i < KEY_BUFFER_SIZE; i++) {
     if (n->keyboard[i].code == code) { return n->keyboard[i].counter; }
   }
   return 0;
 }
-void nibl_setfg(struct Nibl *n, uint8_t r, uint8_t g, uint8_t b, uint8_t a) { n->fgr = r; n->fgg = g; n->fgb = b; n->fga = a; }
-void nibl_setbg(struct Nibl *n, uint8_t r, uint8_t g, uint8_t b, uint8_t a) { n->bgr = r; n->bgg = g; n->bgb = b; n->bga = a; }
-void nibl_background(struct Nibl *n, uint8_t r, uint8_t g, uint8_t b) {
-  SDL_SetRenderDrawColor(n->renderer, r, g, b, 255);   SDL_RenderClear(n->renderer);
+
+void
+nibl_setfg(struct Nibl *n, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+  n->fgr = r; n->fgg = g; n->fgb = b; n->fga = a;
 }
-void nibl_line(struct Nibl *n, int32_t x1, int32_t y1, int32_t x2, int32_t y2) {
+
+void
+nibl_setbg(struct Nibl *n, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+  n->bgr = r; n->bgg = g; n->bgb = b; n->bga = a;
+}
+
+void
+nibl_background(struct Nibl *n, uint8_t r, uint8_t g, uint8_t b)
+{
+  SDL_SetRenderDrawColor(n->renderer, r, g, b, 255);
+  SDL_RenderClear(n->renderer);
+}
+
+void
+nibl_line(struct Nibl *n, int32_t x1, int32_t y1, int32_t x2, int32_t y2)
+{
   SDL_SetRenderDrawColor(n->renderer, n->fgr, n->fgg, n->fgb, n->fga);
   SDL_RenderDrawLine(n->renderer, x1, y1, x2*NIBL_ZOOM, y2*NIBL_ZOOM);
 }
-void nibl_triangle(struct Nibl *n, int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3) {
+
+void
+nibl_triangle(struct Nibl *n, int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3)
+{
   SDL_Point pts[3] = { { x1, y1 }, { x2*NIBL_ZOOM, y2*NIBL_ZOOM }, { x3*NIBL_ZOOM, y3*NIBL_ZOOM } };
   SDL_SetRenderDrawColor(n->renderer, n->fgr, n->fgg, n->fgb, n->fga);
   SDL_RenderDrawLines(n->renderer, pts, 3);
 }
-void nibl_box(struct Nibl *n, int32_t x, int32_t y, int32_t h, int32_t w, int32_t fill) {
+
+void
+nibl_box(struct Nibl *n, int32_t x, int32_t y, int32_t h, int32_t w, int32_t fill)
+{
   SDL_Rect r = { .x = x, .y = y, .w = (w*NIBL_ZOOM), .h = (h*NIBL_ZOOM) };
   if (fill) {
     SDL_SetRenderDrawColor(n->renderer, n->bgr, n->bgg, n->bgb, n->bga);
@@ -127,16 +200,22 @@ void nibl_box(struct Nibl *n, int32_t x, int32_t y, int32_t h, int32_t w, int32_
   SDL_SetRenderDrawColor(n->renderer, n->fgr, n->fgg, n->fgb, n->fga);
   SDL_RenderDrawRect(n->renderer, &r);
 }
-void nibl_point(struct Nibl *n, int32_t x, int32_t y) {
+
+void nibl_point(struct Nibl *n, int32_t x, int32_t y)
+{
   SDL_SetRenderDrawColor(n->renderer, n->fgr, n->fgg, n->fgb, n->fga);
   SDL_Rect r = { .x = x*NIBL_ZOOM, .y = y*NIBL_ZOOM, .w = NIBL_ZOOM, .h = NIBL_ZOOM };
   SDL_RenderDrawRect(n->renderer, &r);
 }
+
 /* Licensed under MIT: https://robey.lag.net/2010/01/23/tiny-monospace-font.html */
 static uint32_t nibl_font[127] = {
-0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x44404, 0xaa, 0xaeaea, 0x6c6c4, 0x82482, 0xccea6, 0x44, 0x24442, 0x84448, 0xa4a, 0x4e4, 0x48, 0xe, 0x4, 0x22488, 0x6aaac, 0x4c444, 0xc248e, 0xc242c, 0xaae22, 0xe8c2c, 0x68eae, 0xe2488, 0xeaeae, 0xeae2c, 0x404, 0x4048, 0x24842, 0xe0e, 0x84248, 0xe2404, 0x4ae86, 0x4aeaa, 0xcacac, 0x68886, 0xcaaac, 0xe8e8e, 0xe8e88, 0x68ea6, 0xaaeaa, 0xe444e, 0x222a4, 0xaacaa, 0x8888e, 0xaeeaa, 0xaeeea, 0x4aaa4, 0xcac88, 0x4aae6, 0xcaeca, 0x6842c, 0xe4444, 0xaaaa6, 0xaaa44, 0xaaeea, 0xaa4aa, 0xaa444, 0xe248e, 0xe888e, 0x842, 0xe222e, 0x4a, 0xe, 0x84, 0xc6ae, 0x8caac, 0x6886, 0x26aa6, 0x6ac6, 0x24e44, 0x6ae24, 0x8caaa, 0x40444, 0x2022a4, 0x8acca, 0xc444e, 0xeeea, 0xcaaa, 0x4aa4, 0xcaac8, 0x6aa62, 0x6888, 0x6c6c, 0x4e446, 0xaaa6, 0xaae4, 0xaeee, 0xa44a, 0xaa624, 0xe6ce, 0x64846, 0x44044, 0xc424c, 0x6c
+  0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x44404, 0xaa, 0xaeaea, 0x6c6c4, 0x82482, 0xccea6, 0x44, 0x24442, 0x84448, 0xa4a, 0x4e4, 0x48, 0xe, 0x4, 0x22488, 0x6aaac, 0x4c444, 0xc248e, 0xc242c, 0xaae22, 0xe8c2c, 0x68eae, 0xe2488, 0xeaeae, 0xeae2c, 0x404, 0x4048, 0x24842, 0xe0e, 0x84248, 0xe2404, 0x4ae86, 0x4aeaa, 0xcacac, 0x68886, 0xcaaac, 0xe8e8e, 0xe8e88, 0x68ea6, 0xaaeaa, 0xe444e, 0x222a4, 0xaacaa, 0x8888e, 0xaeeaa, 0xaeeea, 0x4aaa4, 0xcac88, 0x4aae6, 0xcaeca, 0x6842c, 0xe4444, 0xaaaa6, 0xaaa44, 0xaaeea, 0xaa4aa, 0xaa444, 0xe248e, 0xe888e, 0x842, 0xe222e, 0x4a, 0xe, 0x84, 0xc6ae, 0x8caac, 0x6886, 0x26aa6, 0x6ac6, 0x24e44, 0x6ae24, 0x8caaa, 0x40444, 0x2022a4, 0x8acca, 0xc444e, 0xeeea, 0xcaaa, 0x4aa4, 0xcaac8, 0x6aa62, 0x6888, 0x6c6c, 0x4e446, 0xaaa6, 0xaae4, 0xaeee, 0xa44a, 0xaa624, 0xe6ce, 0x64846, 0x44044, 0xc424c, 0x6c
 };
-static void nibl_drawchar(struct Nibl *n, char c, int32_t x, int32_t y) {
+
+static void
+nibl_drawchar(struct Nibl *n, char c, int32_t x, int32_t y)
+{
   uint32_t fd = nibl_font[(int32_t)c]; int count = 0; int xo = 0; int yo = 0;
   SDL_Rect rects[24] = {0};
   for (int i = 23; i >= 0; i--) { /* backgrounds from bottom right corner */
@@ -147,9 +226,14 @@ static void nibl_drawchar(struct Nibl *n, char c, int32_t x, int32_t y) {
     }
     fd >>= 1;
   }
-  if (count > 0) { SDL_SetRenderDrawColor(n->renderer, n->fgr, n->fgg, n->fgb, n->fga); SDL_RenderFillRects(n->renderer, rects, count); }
+  if (count > 0) {
+    SDL_SetRenderDrawColor(n->renderer, n->fgr, n->fgg, n->fgb, n->fga);
+    SDL_RenderFillRects(n->renderer, rects, count);
+  }
 }
-void nibl_text(struct Nibl *n, char *s, int32_t x, int32_t y)
+
+void
+nibl_text(struct Nibl *n, char *s, int32_t x, int32_t y)
 {
   int32_t cx = x;
   while (*s) {
